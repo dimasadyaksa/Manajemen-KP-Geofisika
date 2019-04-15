5398592ff824f74eb3e43435845cacf8edbd3d65
plotan <dnndr07@gmail.com>
2019-04-15 21:06:07 +0700

    Update v_daftar_logbook.php

--- application/views/pembimbing_d/v_daftar_logbook.php
+++ application/views/pembimbing_d/v_daftar_logbook.php
@@ -2,35 +2,6 @@
 	<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-
-
-    <div class="container content">
-    <div class="row"><div class="col-md-3">
-
-  <div class="list-group">
-    <a href="http://siakad.itera.ac.id/mahasiswa" class="list-group-item"><i class="fa fa-home tab10" aria-hidden="true"></i>Dashboard</a>
-    <a href="#" class="list-group-item "><i class="fa fa-plus-circle tab10" aria-hidden="true"></i>Profil</a>
-
-  </div>
-
-  <div class="list-group">
-    <a href="#" class="list-group-item list-group-item-header">Menu</a>
-    <a href="#" class="list-group-item">
-      <i class="fa fa-clock-o tab10" aria-hidden="true"></i> Penilaian</a>
-    <a href="#" class="list-group-item ">
-      <i class="fa fa-question-circle tab10" aria-hidden="true"></i> Pengajuan</a>
-    <a href="<?php echo site_url('Pembimbing_Dosen/daftar_logbook')?>" class="list-group-item active">
-      <i class="fa fa-calendar-check-o tab10" aria-hidden="true"></i> Logbook</a>
-    <a href="<?php echo site_url('Pembimbing_Dosen/daftar_mahasiswa')?>" class="list-group-item">
-      <i class="fa fa-list-ul tab10" aria-hidden="true"></i> Daftar Mahasiwa</a>
-    
-  </div>
-
-  
-</div>
-        
-
-
 <div class="col-md-9">
 
 
@@ -57,44 +28,20 @@
         <td>14116001</td>
         <td style="text-align: left">Retno Monika</td>
         <td>2016</td>
-        <td><a href="<?php echo site_url('Logbook_Mahasiswa/Retno_Monika')?>">
-          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Lihat</button></a></td>
-      </tr>
-
-      <tr>
-       <td>2</td>
-        <td>14116002</td>
-        <td style="text-align: left">Ali Musaldi</td>
-        <td>2016</td>
         <td><a href="<?php echo site_url('Pembimbing_Dosen/logbook')?>">
-          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Lihat</button></a></td>
-        </tr>  
-      
-      <tr>
-        <td>3</td>
-        <td>14116003</td>
-        <td style="text-align: left">Sandi Risky</td>
-        <td>2016</td>
-        <td><a href="<?php echo site_url('Pembimbing_Dosen/logbook')?>">
-          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Lihat</button></a></td>
-      </tr>
-
-      <tr>
-        <td>4</td>
-        <td>14116004</td>
-        <td style="text-align: left">Mimi Setiasih</td>
-        <td>2016</td>
-        <td><a href="<?php echo site_url('Pembimbing_Dosen/logbook')?>">
-          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Lihat</button></a></td>
+          <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Lihat</button></a></td>
+        <!-- <td><a href="<?php echo site_url('Logbook_Mahasiswa/Retno_Monika')?>">
+          <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Lihat</button>
+        </a></td> -->
       </tr>
 
        <tr>
-        <td>5</td>
+        <td>2</td>
         <td style="text-align: left"></td>
         <td></td>
         <td></td>
         <td><a href="<?php echo site_url('Pembimbing_Dosen/logbook')?>">
-          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Lihat</button></a></td>
+          <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Lihat</button></a></td>
       </tr>
     </tbody>
   </table>
