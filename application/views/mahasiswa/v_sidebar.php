7914dc4d68db89e3d034b28dd16aa7220f30dd4e
plotan <dnndr07@gmail.com>
2019-04-15 21:05:02 +0700

    Update v_sidebar.php

--- application/views/mahasiswa/v_sidebar.php
+++ application/views/mahasiswa/v_sidebar.php
@@ -3,8 +3,8 @@
 
   <div class="list-group">
     <a href="http://siakad.itera.ac.id/mahasiswa" class="list-group-item active"><i class="fa fa-home tab10" aria-hidden="true"></i>Dashboard</a>
-    <a href="#" class="list-group-item "><i class="fa fa-plus-circle tab10" aria-hidden="true"></i>Profil</a>
-
+    <a href="#" class="list-group-item "><i class="fa fa-user-circle tab10" aria-hidden="true"></i>Profil</a>
+    <a href="#" class="list-group-item "><i class="fa fa-plus-circle tab10" aria-hidden="true"></i>Upload Laporan KP</a>
   </div>
 
   <div class="list-group">
@@ -13,11 +13,26 @@
       <i class="fa fa-clock-o tab10" aria-hidden="true"></i> Data Diri </a>
     <a href="<?php echo site_url('Mahasiswa/LogBook')?>" class="list-group-item ">
       <i class="fa fa-question-circle tab10" aria-hidden="true"></i> Log Book </a>
+      <a href="<?php echo site_url('Mahasiswa/TPraktik')?>" class="list-group-item "><i class="fa fa-calendar tab10" aria-hidden="true"></i>Tempat Kerja Praktik</a>
   </div>
 
   <div class="list-group">
     <a href="#" class="list-group-item list-group-item-header">Sistem</a>  
     <a href="<?php echo site_url('Mahasiswa/PLapangan')?>"" class="list-group-item "><i class="fa fa-user-circle tab10" aria-hidden="true"></i>Pembimbing Lapangan</a>
-    <a href="<?php echo site_url('Mahasiswa/TPraktik')?>" class="list-group-item "><i class="fa fa-calendar tab10" aria-hidden="true"></i>Tempat Kerja Praktik</a>
+    
   </div>
+
+  <div class="list-group">
+    <a href="#" class="list-group-item list-group-item-header">Seminar</a>  
+    <a href="<?php echo site_url('Mahasiswa/PLapangan')?>"" class="list-group-item "><i class="fa fa-user-circle tab10" aria-hidden="true"></i>Daftar Seminar</a>
+  </div>
+
+  <div class="list-group">
+    <a href="#" class="list-group-item list-group-item-header">Unduh</a>  
+    <a href="<?php echo site_url('Mahasiswa/PLapangan')?>"" class="list-group-item "><i class="fa fa-user-circle tab10" aria-hidden="true"></i>Formulir Pendaftaran Seminar</a>
+    <a href="<?php echo site_url('Mahasiswa/PLapangan')?>"" class="list-group-item "><i class="fa fa-user-circle tab10" aria-hidden="true"></i>Formulir Permohonan KP</a>
+    <a href="<?php echo site_url('Mahasiswa/TPraktik')?>" class="list-group-item "><i class="fa fa-calendar tab10" aria-hidden="true"></i>Kartu Bimbingan KP</a>
+    <a href="<?php echo site_url('Mahasiswa/TPraktik')?>" class="list-group-item "><i class="fa fa-calendar tab10" aria-hidden="true"></i>Kartu Seminar KP</a>
+  </div>
+
 </div>
